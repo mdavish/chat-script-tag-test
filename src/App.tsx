f5878f5a630fbc9f3af9455cb1e01ed2bef87e78
Maxwell Davish <mdavish@yext.com>
2023-06-09 15:37:50 -0400

    creating the component that we'll make into a script tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChatPopUp } from "@yext/chat-ui-react"
+import { ChatHeadlessProvider } from "@yext/chat-headless-react";
+import "@yext/chat-ui-react/bundle.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export interface ScriptTagProps {
+  apiKey: string;
+  botId: string;
+  title: string;
+  saveToSessionStorage?: boolean;
+  placeholder?: string;
+  showRestartButton?: boolean;
+  stream?: boolean;
+}
 
+function App({
+  apiKey,
+  botId,
+  title,
+  saveToSessionStorage = false,
+  placeholder = "Type a message...",
+  showRestartButton = false,
+  stream = true,
+}: ScriptTagProps) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ChatHeadlessProvider
+      config={{
+        saveToSessionStorage,
+        botId,
+        apiKey,
+      }}
+    >
+      <ChatPopUp
+        title={title}
+        placeholder={placeholder}
+        showRestartButton={showRestartButton}
+        stream={stream}
+      />
+    </ChatHeadlessProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-import "@yext/search-ui-react/bundle.css";
-import { ChatHeadlessProvider } from '@yext/chat-headless-react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChatHeadlessProvider
-      config={{
-        botId: "ski-warehouse-chat",
-        apiKey: "ba41c60c65d874c5340985ad4fcda69a",
-      }}
-    >
-      <App />
-    </ChatHeadlessProvider>
+    <App
+      apiKey="ba41c60c65d874c5340985ad4fcda69a"
+      botId='ski-warehouse-chat'
+      title='Ski Warehouse'
+      stream={false}
+    />
   </React.StrictMode>,
 )
