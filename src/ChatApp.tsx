4efa4a4c487232becec70524587887808ad7530b
Maxwell Davish <mdavish@yext.com>
2023-06-12 15:42:34 -0400

    adding targetable css classes

--- src/ChatApp.tsx
+++ src/ChatApp.tsx
@@ -22,6 +22,7 @@ function ChatApp({
   showRestartButton = false,
   stream = true,
 }: ScriptTagProps) {
+
   return (
     <ChatHeadlessProvider
       config={{
@@ -35,6 +36,42 @@ function ChatApp({
         placeholder={placeholder}
         showRestartButton={showRestartButton}
         stream={stream}
+        customCssClasses={{
+          panel: "yext-chat-panel",
+          panel__hidden: "yext-chat-panel__hidden",
+          panel__display: "yext-chat-panel__display",
+          panelCssClasses: {
+            container: "yext-chat-panel__container",
+            inputContainer: "yext-chat-panel__input-container",
+            inputCssClasses: {
+              container: "yext-chat-panel__input",
+              textArea: "yext-chat-panel__input__text-area",
+              sendButton: "yext-chat-panel__input__send-button",
+            },
+            messageBubbleCssClasses: {
+              message: "yext-chat-panel__message-bubble",
+              message__bot: "yext-chat-panel__message-bubble__bot",
+              message__user: "yext-chat-panel__message-bubble__user",
+              subContainer: "yext-chat-panel__message-bubble__sub-container",
+              subContainer__bot: "yext-chat-panel__message-bubble__sub-container__bot",
+              subContainer__user: "yext-chat-panel__message-bubble__sub-container__user",
+              timestamp: "yext-chat-panel__message-bubble__timestamp",
+              timestamp__bot: "yext-chat-panel__message-bubble__timestamp__bot",
+              timestamp__user: "yext-chat-panel__message-bubble__timestamp__user",
+              topContainer: "yext-chat-panel__message-bubble__top-container",
+            }
+          },
+          button: "yext-chat-button",
+          button__display: "yext-chat-button__display",
+          button__hidden: "yext-chat-button__hidden",
+          container: "yext-chat-container",
+          headerCssClasses: {
+            header: "yext-chat-header",
+            closeButton: "yext-chat-header__close-button",
+            restartButton: "yext-chat-header__restart-button",
+            title: "yext-chat-header__title",
+          }
+        }}
       />
     </ChatHeadlessProvider>
   )
